3245e42b2c4914ea79027013bdae3e2483cbdbce
pablo <pablolopezviqueira@gmail.com>
2014-05-13 22:00:13 +0200

    nikola testing

--- conf.py
+++ conf.py
@@ -8,16 +8,16 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Your Name"
-BLOG_TITLE = "Demo Site"
+BLOG_AUTHOR = "Pablo López Viqueira"
+BLOG_TITLE = "Just another coder"
 # This is the main URL for your site. It will be used
 # in a prominent link
-SITE_URL = "http://getnikola.com/"
+SITE_URL = "http://tak30.github.io"
 # This is the URL where nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://getnikola.com/"
-BLOG_EMAIL = "joe@demo.site"
-BLOG_DESCRIPTION = "This is a demo site for Nikola."
+BLOG_EMAIL = "pablolopezviqueira@gmail.com"
+BLOG_DESCRIPTION = "This is my personal website"
 
 # Nikola is multilingual!
 #
@@ -130,12 +130,10 @@ NAVIGATION_LINKS = {
 #
 
 POSTS = (
-("posts/*.rst", "posts", "post.tmpl"),
-("posts/*.txt", "posts", "post.tmpl"),
+("posts/*.md", "posts", "post.tmpl"),
 )
 PAGES = (
-("stories/*.rst", "stories", "story.tmpl"),
-("stories/*.txt", "stories", "story.tmpl"),
+("stories/*.md", "stories", "story.tmpl"),
 )
 
 # One or more folders containing files to be copied as-is into the output.
---
+++ posts/starting-with-nikola.md
@@ -0,0 +1,11 @@
+<!-- 
+.. title: Starting with Nikola
+.. slug: starting-with-nikola
+.. date: 2014/05/13 21:45:38
+.. tags: 
+.. link: 
+.. description: 
+.. type: text
+-->
+
+Write your post here.
